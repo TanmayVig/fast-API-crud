c9e36c49fdb3865b66cb0c8ac85e6d113f280f92
Tanmay Vig <tanmayvij11@gmail.com>
2021-01-29 00:40:14 +0530

    optimized adding data, optimization for fetching data required

--- main.py
+++ main.py
@@ -7,10 +7,26 @@ app = FastAPI()
 
 db=[]
 
+def bin_search(arr: list, low: int, high: int, x: int) -> int:
+    mid = int((high - low)/2)
+    if arr[mid].id == x:
+        return mid
+    else:
+        if high > low:
+            if arr[mid].id > x:
+                return bin_search(arr,low, mid-1, x)
+            elif arr[mid].id < x:
+                return bin_search(arr, mid+1, high, x)
+        else:
+            return -1
+
 class Todo(BaseModel):
-    id: int
+    id: int 
     target_head: str
     target: str
+
+    def __str__(self) -> str:
+        return self.target_head
 
 
 @app.get("/")
@@ -21,17 +37,23 @@ def index():
 
 @app.post("/todo")
 def set_task(task: Todo):
+    task.id = db[-1].id + 1 if len(db) !=0 else 0
     db.append(task)
-    return{"added":"true"}
+    return{"added":task}
 
 @app.put("/todo")
 def change_task(id: int, new_target_head: str, new_target: str):
-    db[id-1].target_head = new_target_head
-    db[id-1].target = new_target
-    return db[id-1]
+    loc = bin_search(db,0, len(db)-1, id)
+    if loc != -1:
+        db[loc].target_head = new_target_head
+        db[loc].target = new_target
+        return db[loc]
+    else:
+        return {"message":"no such object"}
 
 
 @app.delete("/todo")
 def delete_task(id: int):
-    res = db.pop(id-1)
+    loc = bin_search(db,0, len(db)-1, id)
+    res = db.pop(loc)
     return res
